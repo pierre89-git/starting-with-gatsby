e2f61e535fefe6f04586644796f4cc8c2bd20fa9
pierre jutand <pierre.jutand@gmail.com>
2021-03-14 16:02:08 +0100

    Some mimor CSS modifications

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,13 @@ const title = "Gatsby"
 
 export default function Home() {
   return (
-    <div style={{ color: "palegoldenrod", fontSize: ".8rem" }}>
-      <Link to="/contact/">Contact</Link>
+   /*{ <div style={{ color: "palegoldenrod", fontSize: ".8rem" }}>}*/
+      <div style={{margin:`3rem auto`, maxWidth: 600}}>
       <Header headerText="About Gastby" />
       <h1>Hello {title}</h1>
       <p>What ...</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <Link to="/contact/">Contact</Link>
     </div>
   )
 }
